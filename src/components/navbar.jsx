58454703cea54f96f8828090aa5d59faf3ddfde0
Akram Idrissi <akram.mo.gb@gmail.com>
2024-09-21 00:15:02 +0100

    Updated z index

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -61,7 +61,7 @@ export default function Navbar() {
                 </div>
             </nav>
             <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
-                <div className="fixed inset-0 z-10" />
+                <div className="fixed inset-0 z-[100]" />
                 <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                     <div className="flex items-center justify-between">
                         <a href="#" className="-m-1.5 p-1.5">
