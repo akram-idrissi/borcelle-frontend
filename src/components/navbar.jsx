2db6193facaf7584f8e6f7f6d73d5f340a0e4ded
Akram Idrissi <akram.mo.gb@gmail.com>
2024-09-27 00:10:56 +0100

    Added a sticky param

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -11,11 +11,11 @@ const navigation = [
     { name: 'Accéssoires', href: '#' },
 ]
 
-export default function Navbar() {
+export default function Navbar({sticky=false}) {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
     return (
-        <header className="sticky top-10 z-50 bg-white/60 backdrop-blur-lg">
+        <header className={`${sticky && "sticky"} top-10 z-50 bg-white/60 backdrop-blur-lg`}>
             <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
                 <div className="flex lg:flex-1">
                     <a href="#" className="-m-1.5 p-1.5">
--- src/components/tape.jsx
+++ src/components/tape.jsx
@@ -6,10 +6,10 @@ import { ChevronDownIcon } from '@heroicons/react/20/solid'
 const currencies = ['CAD', 'USD', 'AUD', 'EUR', 'GBP']
 
 
-export default function Tape() {
+export default function Tape({sticky=false}) {
 
     return (
-        <div className="bg-[#ffe4f3] sticky top-0 z-50">
+        <div className={`bg-[#ffe4f3] ${sticky && "sticky"} top-0 z-50`}>
             <div className="mx-auto flex h-10 max-w-7xl items-center justify-center px-4 sm:px-6 lg:px-8">
                 <p className="flex-1 text-center text-sm font-bold lg:flex-none">
                     Livraison Gratuite à partir de 350 DH
