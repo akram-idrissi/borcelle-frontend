dcfc7085b0a6d475674084c99a74fba0f5942693
Akram Idrissi <akram.mo.gb@gmail.com>
2024-10-04 15:19:50 +0100

    Updated imageSrc

--- src/app/categorie/[name]/page.js
+++ src/app/categorie/[name]/page.js
@@ -63,7 +63,7 @@ const products = [
     price: '$256',
     description: 'Get the full lineup of our Basic Tees. Have a fresh shirt all week, and an extra for laundry day.',
     options: '8 colors',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-01.jpg',
+    imageSrc: '/categories/yeux.png',
     imageAlt: 'Eight shirts arranged on table in black, olive, grey, blue, white, red, mustard, and green.',
   },
   {
@@ -73,7 +73,7 @@ const products = [
     price: '$32',
     description: 'Look like a visionary CEO and wear the same black t-shirt every day.',
     options: 'Black',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-02.jpg',
+    imageSrc: 'https://tailwindui.com/plus/img/ecommerce-images/category-page-02-image-card-02.jpg',
     imageAlt: 'Front of plain black t-shirt.',
   },
   // More products...
