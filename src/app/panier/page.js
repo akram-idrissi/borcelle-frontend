e13f08aa8ac8ecbfb12f78c72dd759c97034cfc2
Akram Idrissi <akram.mo.gb@gmail.com>
2024-11-09 11:16:57 +0100

    Hid the tape

--- src/app/panier/page.js
+++ src/app/panier/page.js
@@ -107,7 +107,7 @@ export default function Panier() {
     <div className="bg-white">
       
       <header className="relative bg-white">
-        <Tape/>
+        {/*<Tape />*/}
         <Navbar/>
       </header>
 
