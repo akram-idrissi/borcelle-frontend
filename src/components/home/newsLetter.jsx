76504a43899afa53c8c98deb41b8ee7e1c9f9b22
Akram Idrissi <akram.mo.gb@gmail.com>
2024-11-18 23:03:02 +0100

    Updated style

--- src/components/home/newsLetter.jsx
+++ src/components/home/newsLetter.jsx
@@ -15,9 +15,9 @@
 export default function NewsLetter() {
     return (
         <>
-            <div className="bg-white pb-32 sm:pb-40">
+            <div className="bg-white px-4 sm:px-0 pb-32 sm:pb-40">
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                    <div className="relative isolate flex justify-between flex-col gap-10 overflow-hidden bg-[#000] px-6 py-24 shadow-2xl sm:rounded-3xl sm:px-24 xl:flex-row xl:items-center xl:py-32">
+                    <div className="relative isolate flex justify-between flex-col gap-10 overflow-hidden bg-[#000] px-6 py-24 shadow-2xl rounded-3xl sm:px-24 xl:flex-row xl:items-center xl:py-32">
                         <div>
                             <h2 className="font-abhaya max-w-2xl text-3xl font-bold tracking-tight text-white sm:text-5xl xl:max-w-none xl:flex-auto">
                                 Get up to 20% off !
