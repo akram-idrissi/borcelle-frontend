1860fb62c7a6c75be03e671375a4f17f3d0bff91
Akram Idrissi <akram.mo.gb@gmail.com>
2024-11-24 14:00:30 +0100

    Updated function

--- src/services/api.js
+++ src/services/api.js
@@ -17,11 +17,11 @@ export const getProductById = async (id) => {
   return result;
 };
 
-export const getProductsByCategory = async (category) => {
+export const getProductsByCategory = async (category, page=0, size=5) => {
   const result = {loading: true, error: null, data: null};
 
   try {
-    const response = await axios.get(`${backendURL}/api/products/category/${category}`);
+    const response = await axios.get(`${backendURL}/api/products/category/${category}?page=${page}&size=${size}`);
     result.data = response.data;
   } catch (error) {
     result.error = error.message || "An error occurred while fetching category products";
