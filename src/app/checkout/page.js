8e83fad10db0c949400ed51632376997b0851a18
Akram Idrissi <akram.mo.gb@gmail.com>
2024-11-24 09:50:25 +0100

    Broke down into small comps

--- src/app/checkout/page.js
+++ src/app/checkout/page.js
@@ -1,181 +1,28 @@
-import { UserIcon } from '@heroicons/react/24/outline'
+import Navbar from '@/components/navbar'
+import Form from './form'
+import MobileOrderSummary from './mobileOrderSummary'
+import OrderSummary from './orderSummary'
 
-const products = [
-  {
-    id: 1,
-    name: "Women's Basic Tee",
-    href: '#',
-    price: '$32.00',
-    color: 'Gray',
-    size: 'S',
-    imageSrc: 'https://tailwindui.com/plus/img/ecommerce-images/checkout-page-05-product-01.jpg',
-    imageAlt: "Front of women's basic tee in heather gray.",
-  },
-  {
-    id: 2,
-    name: "Women's Basic Tee",
-    href: '#',
-    price: '$32.00',
-    color: 'Gray',
-    size: 'S',
-    imageSrc: 'https://tailwindui.com/plus/img/ecommerce-images/checkout-page-05-product-02.jpg',
-    imageAlt: "Front of women's basic tee in heather gray.",
-  },
-  {
-    id: 3,
-    name: "Women's Basic Tee",
-    href: '#',
-    price: '$32.00',
-    color: 'Gray',
-    size: 'S',
-    imageSrc: 'https://tailwindui.com/plus/img/ecommerce-images/checkout-page-05-product-03.jpg',
-    imageAlt: "Front of women's basic tee in heather gray.",
-  },
-  // More products...
-]
 
-export default function Example() {
+export default function Checkout() {
   return (
-    <div className="bg-white">
-      <header className="flex items-center justify-between px-4 py-10 sm:px-6 sm:py-8 lg:px-8">
-        <a href="#">
-          <span className="sr-only">Your Company</span>
-          <img alt="" src="/logo.png" className="h-8 w-auto" />
-        </a>
-        <div className="hidden sm:flex sm:items-center sm:space-x-8">
-          <a href="#" className="text-sm font-medium text-gray-700">
-            Contacter le support
-          </a>
-          <a href="#" className="-m-2 p-2 text-gray-400 hover:text-gray-500">
-            <span className="sr-only">Account</span>
-            <UserIcon aria-hidden="true" className="h-6 w-6" />
-          </a>
-        </div>
-      </header>
+    <>
+      <Navbar sticky={true} />
+      <main className="lg:flex lg:min-h-full lg:flex-row-reverse lg:overflow-hidden">
+        
 
-      <main className="mx-auto max-w-7xl px-4 pb-16 pt-4 sm:px-6 sm:pb-24 sm:pt-8 lg:px-8 xl:px-2 xl:pt-14">
         <h1 className="sr-only">Checkout</h1>
 
-        <div className="mx-auto grid max-w-lg grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-2">
-          <div className="mx-auto w-full max-w-lg">
-            <h2 className="sr-only">Order summary</h2>
+        {/* Mobile order summary */}
+        <MobileOrderSummary />
+        
 
-            <div className="flow-root">
-              <ul role="list" className="-my-6 divide-y divide-gray-200">
-                {products.map((product) => (
-                  <li key={product.id} className="flex space-x-6 py-6">
-                    <img
-                      alt={product.imageAlt}
-                      src={product.imageSrc}
-                      className="h-24 w-24 flex-none rounded-md bg-gray-100 object-cover object-center"
-                    />
-                    <div className="flex-auto">
-                      <div className="space-y-1 sm:flex sm:items-start sm:justify-between sm:space-x-6">
-                        <div className="flex-auto space-y-1 text-sm font-medium">
-                          <h3 className="text-gray-900">
-                            <a href={product.href}>{product.name}</a>
-                          </h3>
-                          <p className="text-gray-900">{product.price}</p>
-                          <p className="hidden text-gray-500 sm:block">{product.color}</p>
-                          <p className="hidden text-gray-500 sm:block">{product.size}</p>
-                        </div>
-                        <div className="flex flex-none space-x-4">
-                          <button type="button" className="text-sm font-medium text-indigo-600 hover:text-indigo-500">
-                            Edit
-                          </button>
-                          <div className="flex border-l border-gray-300 pl-4">
-                            <button type="button" className="text-sm font-medium text-indigo-600 hover:text-indigo-500">
-                              Remove
-                            </button>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            </div>
+        {/* Order summary */}
+        <OrderSummary />
 
-            <dl className="mt-10 space-y-6 text-sm font-medium text-gray-500">
-              <div className="flex justify-between">
-                <dt>Subtotal</dt>
-                <dd className="text-gray-900">$104.00</dd>
-              </div>
-              <div className="flex justify-between">
-                <dt>Taxes</dt>
-                <dd className="text-gray-900">$8.32</dd>
-              </div>
-              <div className="flex justify-between">
-                <dt>Shipping</dt>
-                <dd className="text-gray-900">$14.00</dd>
-              </div>
-              <div className="flex justify-between border-t border-gray-200 pt-6 text-gray-900">
-                <dt className="text-base">Total</dt>
-                <dd className="text-base">$126.32</dd>
-              </div>
-            </dl>
-          </div>
-
-          <div className="mx-auto w-full max-w-lg">
-            <form className="">
-              <h2 className="text-4xl font-medium text-gray-900">Contact information</h2>
-
-              <div className="mt-6">
-                <label htmlFor="email-address" className="block text-sm font-medium text-gray-700">
-                  Adresse email 
-                </label>
-                <div className="mt-1">
-                  <input
-                    id="email-address"
-                    name="email-address"
-                    type="email"
-                    autoComplete="email"
-                    className="px-3 py-2 border block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                  />
-                </div>
-              </div>
-
-              <div className="mt-6">
-                <label htmlFor="phone" className="block text-sm font-medium text-gray-700">
-                  Téléphone
-                </label>
-                <div className="mt-1">
-                  <input
-                    id="phone"
-                    name="phone"
-                    type="tel"
-                    autoComplete="tel"
-                    className="px-3 py-2 border  block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                  />
-                </div>
-              </div>
-
-              <div className="mt-6">
-                <label htmlFor="email-address" className="block text-sm font-medium text-gray-700">
-                  Adresse
-                </label>
-                <div className="mt-1">
-                  <input
-                    id="email-address"
-                    name="email-address"
-                    type="texte"
-                    autoComplete="email"
-                    className="px-3 py-2 border block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                  />
-                </div>
-              </div>
-
-              <button
-                type="submit"
-                disabled
-                className="mt-6 w-full rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-gray-100 disabled:text-gray-500"
-              >
-                Envoyer la commande 
-              </button>
-            </form>
-          </div>
-        </div>
+        {/* Checkout form */}
+        <Form />
       </main>
-    </div>
+    </>
   )
 }
