2928ce75aec2bee158a038be53f02bc0db9edd29
Akram Idrissi <akram.mo.gb@gmail.com>
2024-11-30 17:42:30 +0100

    Validation for signup

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -1,5 +1,26 @@
+'use client'
+
+import { useState } from "react";
+import { validateEmail, validatePassword, validateUsername } from "../signin/form-validation";
 
 export default function Signup() {
+    const [errors, setErrors] = useState({});
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSignupForm = async (event) => {
+        event.preventDefault();
+
+        let usernameError = validateUsername(username);
+        let emailError = validateEmail(email);
+        let passwordError = validatePassword(password);
+        
+        if (usernameError || passwordError || emailError) {
+            setErrors({username: usernameError, email: emailError, password: passwordError});
+            return;
+        }
+    }
     return (
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -14,21 +35,22 @@ export default function Signup() {
             </div>
 
             <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                <form action="#" method="POST" className="space-y-6">
+                <form onSubmit={handleSignupForm} method="POST" className="space-y-6">
                     <div>
-                        <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                        <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
                             Username
                         </label>
                         <div className="mt-2">
                             <input
-                                id="email"
-                                name="email"
+                                id="username"
+                                name="username"
                                 type="text"
-                                required
-                                autoComplete="email"
+                                onChange={(event) => {setErrors({...errors, username: ""}); setUsername(event.target.value)}}
+                                autoComplete="username"
                                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6"
                             />
                         </div>
+                        {errors.username && <div className="text-xs text-red-500">{errors.username}</div>  }
                     </div>
 
                     <div>
@@ -40,11 +62,12 @@ export default function Signup() {
                                 id="email"
                                 name="email"
                                 type="text"
-                                required
+                                onChange={(event) => {setErrors({...errors, email: ""}); setEmail(event.target.value)}}
                                 autoComplete="email"
                                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6"
                             />
                         </div>
+                        {errors.email && <div className="text-xs text-red-500">{errors.email}</div>  }
                     </div>
 
                     <div>
@@ -58,11 +81,12 @@ export default function Signup() {
                                 id="password"
                                 name="password"
                                 type="password"
-                                required
+                                onChange={(event) => {setErrors({...errors, password: ""}); setPassword(event.target.value)}}
                                 autoComplete="current-password"
                                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6"
                             />
                         </div>
+                        {errors.password && <div className="text-xs text-red-500">{errors.password}</div>  }
                     </div>
 
                     <div>
