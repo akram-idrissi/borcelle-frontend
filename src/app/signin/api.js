c81a1ffc51e0c9eed61c914bf95c8e426b9738e1
Akram Idrissi <akram.mo.gb@gmail.com>
2024-12-07 16:10:59 +0100

    Small changes

--- src/app/signin/api.js
+++ src/app/signin/api.js
@@ -6,9 +6,12 @@ export const login = async (loginData) => {
     const result = {loading: true, error: null, data: null};
 
     try {
-        let loginURL = `${base_url}/auth/login`; 
-        const response = await axios.post(loginURL, loginData, { withCredentials: true });
+        let loginURL = `${base_url}/auth-service/public/auth/login`;
+        const response = await axios.post(loginURL, loginData, { withCredentials: true,headers: {
+                'Content-Type': 'application/json',
+            } });
         result.data = response.data;
+        console.log(result.data);
     } catch (error) {
         console.error("Error Signin in: ", error);
         result.error = error.message || "An error occurred while signin in";
